ef3d2e3432220fb9334b57e7bfed99efad092eea
danishkodemonkey <humberjini@hotmail.com>
2024-08-07 14:59:42 +0200

    express app entrypoint

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const usersRouter = require('./routes/usersRouter');
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', usersRouter);
+
+const port = process.env.port || 3000;
+app.listen(port, () => {
+    console.log('Express data handling launched, listening on ', port);
+});
